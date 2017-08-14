4cba12b6ba42e6a7bc2659b41bef8e347b0a73a9
ankit <ankit.chachada@gmail.com>
2017-08-14 10:36:05 -0400

    fixed testcase

--- spec/requests/booking_controller_spec.rb
+++ spec/requests/booking_controller_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "bookings", type: :request do
   
   describe "GET api/bookings" do
     it "get the list of all bookings" do
-      Fabricate(:booking)
+      assignment = Fabricate(:assignment)
+      timeslot = assignment.timeslot
+      booking = Booking.create(timeslot_id: timeslot.id, size: 6)
       get api_bookings_path
       expect(json).not_to be_empty
       expect(json.size).to eq(1)
