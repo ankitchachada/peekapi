796b792a6af39590a8d666549ac20868e1252a75
Ankit Chachada <ankit.chachada@gmail.com>
2017-08-15 20:42:13 -0400

    update booking validator indentation

--- app/validators/booking_validator.rb
+++ app/validators/booking_validator.rb
@@ -1,12 +1,10 @@
 class BookingValidator < ActiveModel::Validator
-
-	def validate(record)
-		#to check overflow booking
-   	timeslot = record.timeslot
-		capacity = timeslot.boats.pluck(:capacity).max
-		if capacity < record.size
-			record.errors.add(:capacity, "Customers cannot be booked beyond capacity")
-		end
-	end
-
+  def validate(record)
+    #to check overflow booking
+    timeslot = record.timeslot
+    capacity = timeslot.boats.pluck(:capacity).max
+    if capacity < record.size
+      record.errors.add(:capacity, "Customers cannot be booked beyond capacity")
+    end
+  end
 end
