5dead78c22edb04fb1051a7981ba38ea34b73456
Ankit Chachada <ankit.chachada@gmail.com>
2017-08-15 20:43:11 -0400

    update assignment validator indentation

--- app/validators/assignment_validator.rb
+++ app/validators/assignment_validator.rb
@@ -1,12 +1,9 @@
 class AssignmentValidator < ActiveModel::Validator
-
-	def validate(record)
-		timeslot = record.timeslot
-		timeslots = record.boat.timeslots.where("timeslots.start_time < ? and timeslots.end_time > ? ",timeslot.end_time,timeslot.start_time)
-		if timeslots.count > 0
-		  record.errors.add(:Assignment, "Boat cannot be assign at this timeslot")
-		end
-	end
-
-
+  def validate(record)
+    timeslot = record.timeslot
+    timeslots = record.boat.timeslots.where("timeslots.start_time < ? and timeslots.end_time > ? ",timeslot.end_time,timeslot.start_time)
+    if timeslots.count > 0
+      record.errors.add(:Assignment, "Boat cannot be assign at this timeslot")
+    end
+  end
 end
