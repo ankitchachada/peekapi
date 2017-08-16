6826a2d906b52fd48f56c292ba85c6c658f835fd
Ankit Chachada <ankit.chachada@gmail.com>
2017-08-15 20:38:41 -0400

    update booking controller indentation

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,20 +1,20 @@
 class Api::BookingsController < ApplicationController
-	def index
-		bookings = Booking.all
-		render json: bookings, status: 200
-	end
-	
-	def create
-		booking = Booking.new(booking_params)
-		if booking.save
-			render json: booking, status: 201
-		else
-			render json: booking.errors, status: :unprocessable_entity 
-		end
-	end
+  def index
+    bookings = Booking.all
+    render json: bookings, status: 200
+  end
 
-	private
-	def booking_params
-		params.require(:booking).permit(:timeslot_id, :size)
-	end
+  def create
+    booking = Booking.new(booking_params)
+    if booking.save
+      render json: booking, status: 201
+    else
+      render json: booking.errors, status: :unprocessable_entity 
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:timeslot_id, :size)
+  end
 end
